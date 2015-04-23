c2152e597135912d7daac5c0e2dbfcbf01cf1f6a
Rafael Tejado <rtejado90@gmail.com>
2015-04-23 18:28:13 +0200

    Add test nº2 to MovieGetter. MovieGetter returns the instance of a movie when a title passed

--- spec/ascii_movies_spec.rb
+++ spec/ascii_movies_spec.rb
@@ -1,10 +1,21 @@
 require_relative('../ascii_movies')
 
 RSpec.describe "Ascii Movies" do
+	let(:ghostbusters) do
+		instance_double("Movie",
+			:title => "Ghostbusters",
+			:rating => 7.8)
+	end
+
 	it "MovieGetter returns an empty string when no film is passed" do
 		movie_getter = MovieGetter.new("")
 		expect(movie_getter.get_movie).to eq("")
 	end
+	it "MovieGetter returns the instance of a movie when a title passed" do
+		movie_getter = MovieGetter.new("Ghostbusters")
+		expect(movie_getter.get_movie).to eq(ghostbusters)
+	end
+
 end
 
 

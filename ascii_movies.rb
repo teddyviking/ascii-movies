ccbd8a76facf3ca66fa10f69c11cd01080bd6cb3
Rafael Tejado <rtejado90@gmail.com>
2015-04-23 18:13:00 +0200

    Test nº1 of MovieGetter passed

--- ascii_movies.rb
+++ ascii_movies.rb
@@ -2,5 +2,11 @@ require 'pry'
 require 'imdb'
 
 class MovieGetter
-	
+	def initialize(movie)
+		@movie = movie
+	end
+
+	def get_movie
+		""
+	end
 end
--- spec/ascii_movies_spec.rb
+++ spec/ascii_movies_spec.rb
@@ -2,8 +2,8 @@ require_relative('../ascii_movies')
 
 RSpec.describe "Ascii Movies" do
 	it "MovieGetter returns an empty string when no film is passed" do
-		movie_getter = MovieGetter.new
-		expect(movie_getter.get_movie("")).to eq("")
+		movie_getter = MovieGetter.new("")
+		expect(movie_getter.get_movie).to eq("")
 	end
 end
 
